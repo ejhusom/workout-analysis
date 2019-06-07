95930068e4fbf0eb7ece6d6382b17dc1691ac8cb
ejhusom <ejlgkvam@online.no>
2019-06-07 12:57:06 +0200

    intial commit

---
+++ treadmill_elevation_gain.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#==========================================================================
+# Program by Erik Johannes B. L. G. Husom on 2018-01-14 for Python 3.6.4
+# Description: Calculating elevation gain on a treadmill
+# USAGE:
+#==========================================================================
+# IMPORT STATEMENTS
+import sys
+import numpy as np
+
+def elevationGain(incline, distance):
+    incline = incline/100
+    elevationGain = distance*np.cos(np.arctan(incline))*incline
+    return elevationGain*1000
+
+def elevationGainRiseOverRun(incline, distance):
+    incline = incline/100
+    elevationGain = distance*incline
+    print(elevationGain*1000)
+
+incline = float(input("Incline (percentage)="))
+distance = float(input("Distance (km)="))
+#speed = float(input("Speed (km/h)="))
+time = float(input("Time (min)="))
+
+gain = elevationGain(incline, distance)
+#time = distance/speed
+time /= 60
+speed = distance/time
+print("Elevation gain: ", gain)
+print("Speed: ", speed)
+print("Vertical speed (m/min): ", gain/(time*60))
+print("Vertical speed (m/hr): ", gain/time)
